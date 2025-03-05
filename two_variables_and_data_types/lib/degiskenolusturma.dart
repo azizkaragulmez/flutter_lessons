ce60c160d91ac65e1ccc474c9d23716c91dd441b
Aziz karagülmez <123738266+azizkaragulmez@users.noreply.github.com>
2025-03-05 12:37:25 +0300

    create variable

---
+++ two_variables_and_data_types/lib/degiskenolusturma.dart
@@ -0,0 +1,27 @@
+void main(){
+
+  var ogrenciAdi="Aziz";
+  var ogrenciYasi=24;
+  var ogrenciBoyu=1.72;
+  var ogrenciBasHarfi="A";
+
+  print(ogrenciAdi);  
+  print (ogrenciYasi);
+  print(ogrenciBoyu);
+  print(ogrenciBasHarfi);
+
+//iki tanımlamada kullanılır, altakinin avantajı sadece türünüde görebiliyoruz
+  int urun_id =3416;
+  String urun_adi= "Kol saati";
+  int urun_adet=100;
+  double urun_fiyat=149.99;
+  String urun_tedarikci="rolex";
+
+  print("Ürün id $urun_id");
+  print("Ürün Adı $urun_adi");
+  print("Ürün adet $urun_adet");
+  print("Ürun fiyat $urun_fiyat");
+  print("Ürün tedarikçi $urun_tedarikci");
+  
+  
+}
